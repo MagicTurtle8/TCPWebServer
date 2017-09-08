8479cc68abf66878c111d5d98c8276d20efe4c42
Peter <peter.luu@student.uts.edu.au>
2017-09-08 10:49:40 +1000

    comit

--- WebServer.py
+++ WebServer.py
@@ -1,28 +1,46 @@
-#import socket module
-from socket import *
+'''
+A web server that handles one HTTP request at a time.
 
+This web server should accept and parse the HTTP request, get the requested file from the server's file system,
+create an HTTP response message consisiting of the requested file preceded by headerlines,
+and then send the response directly to the client.
+
+If the requested file is not present to the server, the server should send an HTTP "404 Not Found" message
+back to the client
+'''
+
+
+from socket import * #import socket module
 import sys # In order to terminate the program
 
-serverSocket = socket(AF_INET, SOCK_STREAM)
 
+serverSocket = socket(AF_INET, SOCK_STREAM)
 #Prepare a server socket
 
-#Fill in start
-
-#Fill in end
+#FILL IN START ------------------------------
+serverName = 'serverName'
+serverPort = 12000
+serverSocket.bind(('', 12000))
+serverSocket.listen(1)
+#FILL IN END ------------------------------
 
 while True:
     #Establish the connection
     print('Ready to serve...')
-    connectionSocket, addr = #Fill in start   #Fill in end
+    connectionSocket, addr = serverSocket.accept()
+
     try:
-        message = #Fill in start    #Fill in end
+        message = connectionSocket.recv(1024)
         filename = message.split()[1]
         f = open(filename[1:])
-        outputdata = #Fill in start    #Fill in end
+        outputdata = f.read()#Fill in start    #Fill in end
+
         #Send one HTTP header line into socket
-        #Fill in start
-        #Fill in end
+
+        # FILL IN START ------------------------------
+        connectionSocket.send('HTTP/1.0 200 OK\r\n\r\n')
+        # FILL IN END --------------------------------
+
         #Send the content of the requested file to the client
         for i in range(0, len(outputdata)):
             connectionSocket.send(outputdata[i].encode())
@@ -30,12 +48,15 @@ while True:
 
         connectionSocket.close()
     except IOError:
-        #Send response message for file not found
-        #FILL IN START
 
-        #FILL IN END
+        # Send response message for file not found
+        # FILL IN START ------------------------------
+        connectionSocket.send('404 Not Found')
+        # FILL IN END --------------------------------
         #Close client socket
-        #FILL in start
-        #FILL in end
+
+        # FILL IN START ------------------------------
+        connectionSocket.close()
+        # FILL IN END --------------------------------
     serverSocket.close()
     sys.exit() #Terminate the program after sending the corresponding data
